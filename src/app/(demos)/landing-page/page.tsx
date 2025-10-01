752b764e31095a6912caddedc54cc479784b1b44
Tabin Sheikh <sheikhtabin@gmail.com>
2025-10-01 21:59:54 +0500

    eslint

--- src/app/(demos)/landing-page/page.tsx
+++ src/app/(demos)/landing-page/page.tsx
@@ -1,17 +1,9 @@
 import React from "react";
 import Footer from "@/components/Footer";
-import 'swiper/css'
-import Swiperslider from "@/components/Swiperslider";
 import Hero from "./components/Hero";
-import Services from "@/components/Services";
 import Partner from "@/components/Partner";
 import Closing from "@/components/Closing";
 import About from "@/components/About";
-import Pricing from "@/components/Pricing";
-import Faqs from "@/components/Faqs";
-import Testimonials from "@/components/Testimonials";
-import Blog from "@/components/Blog";
-import Contact from "@/components/Contact";
 import Navigation from "@/components/Navigation";
 import Waitlist from "@/components/Waitlist";
 import Features from "@/components/Features";
